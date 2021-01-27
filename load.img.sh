fdbfe660b8f53910d7c11f82d55adc0c3c5a30fb
numbnet <zusyurec@gmail.com>
2021-01-27 18:23:56 +0200

    111111111111
    2222222222222

---
+++ load.img.sh
@@ -0,0 +1,11 @@
+wget https://github.com/numbnet/OEMDRV/releases/download/CentOS.v7.8.2003/initrd.img ./images/pxeboot/initrd.img
+wget https://github.com/numbnet/OEMDRV/releases/download/CentOS.v7.8.2003/initrd.img ./isolinux/initrd.img
+wget https://github.com/numbnet/OEMDRV/releases/download/CentOS.v7.8.2003/squashfs.img ./LiveOS/squashfs.img
+
+wget https://github.com/numbnet/OEMDRV/releases/download/Packages/linux-firmware-20191203-76.gite8a0f4c.el7.noarch.rpm ./Packages/linux-firmware-20191203-76.gite8a0f4c.el7.noarch.rpm
+wget https://github.com/numbnet/OEMDRV/releases/download/Packages/kernel-3.10.0-1127.el7.x86_64.rpm ./Packages/kernel-3.10.0-1127.el7.x86_64.rpm
+wget https://github.com/numbnet/OEMDRV/releases/download/Packages/linux-firmware-20191203-76.gite8a0f4c.el7.noarch.rpm ./Packages/linux-firmware-20191203-76.gite8a0f4c.el7.noarch.rpm
+wget https://github.com/numbnet/OEMDRV/releases/download/Packages/centos-logos-70.0.6-3.el7.centos.noarch.rpm ./Packages/centos-logos-70.0.6-3.el7.centos.noarch.rpm
+
+
+
